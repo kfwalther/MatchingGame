6d22e4d2f71f17892c144ea33f57944f2db08011
walther.PC <kfwalther@gmail.com>
2019-03-24 16:32:15 -0400

    First commit of the Matching Game C#, with the first couple steps implemented.

--- MatchingGame/Form1.cs
+++ MatchingGame/Form1.cs
@@ -12,9 +12,64 @@ namespace MatchingGame
 {
     public partial class Form1 : Form
     {
+
+        // Use this Random object to choose random icons for the squares.
+        Random random = new Random();
+
+        // Each of these letters is an interesting icon in the Webdings font,
+        // and each icon appears twice in this list.
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
         public Form1()
         {
             InitializeComponent();
+            // Assign each square a random icon.
+            AssignIconsToSquares();
+        }
+
+        /// <summary>
+        /// Assign each icon from the list of icons to a random square.
+        /// </summary>
+        private void AssignIconsToSquares()
+        {
+            /** The TableLayoutPanel has 16 labels, and the icon list has 16 icons,
+                so an icon is pulled at random from the list and added to each label. */
+            foreach (Control control in this.tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {   
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method is an event handler for every label's Click event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void label_Click(object sender, EventArgs e)
+        {
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel != null)
+            {
+                // If the clicked label is already black, do nothing.
+                if (clickedLabel.ForeColor == Color.Black)
+                {
+                    return;
+                }
+                // Change the clicked label to black to reveal the icon.
+                clickedLabel.ForeColor = Color.Black;
+            }
         }
     }
 }
--- MatchingGame/Program.cs
+++ MatchingGame/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/**
+ * @author: K. Walther
+ * @date: Mar 2019
+ * @brief: This program implements a Matching Game based on the tutorial on the Microsoft website:
+ * https://docs.microsoft.com/en-us/visualstudio/ide/tutorial-3-create-a-matching-game?view=vs-2017
+ * 
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
