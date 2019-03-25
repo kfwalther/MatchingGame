ef0e46ee14d906c3fd30f98e3f89710eb54a5e79
walther.PC <kfwalther@gmail.com>
2019-03-24 21:55:27 -0400

    Finishing up the matching game functionality, based on the tutorial.

--- MatchingGame/Form1.cs
+++ MatchingGame/Form1.cs
@@ -24,11 +24,16 @@ namespace MatchingGame
             "b", "b", "v", "v", "w", "w", "z", "z"
         };
 
+        // This points to the first label control that the player chooses, and will be null if nothing is clicked yet.
+        Label firstClicked = null;
+        // This points to the second label control the player clicks.
+        Label secondClicked = null;
+
         public Form1()
         {
             InitializeComponent();
             // Assign each square a random icon.
-            AssignIconsToSquares();
+            this.AssignIconsToSquares();
         }
 
         /// <summary>
@@ -43,12 +48,38 @@ namespace MatchingGame
                 Label iconLabel = control as Label;
                 if (iconLabel != null)
                 {   
-                    int randomNumber = random.Next(icons.Count);
-                    iconLabel.Text = icons[randomNumber];
+                    int randomNumber = this.random.Next(this.icons.Count);
+                    iconLabel.Text = this.icons[randomNumber];
                     iconLabel.ForeColor = iconLabel.BackColor;
-                    icons.RemoveAt(randomNumber);
+                    this.icons.RemoveAt(randomNumber);
                 }
             }
+        }
+
+        /// <summary>
+        /// Check if the user has won by matching all of the icons.
+        /// </summary>
+        private void CheckForWinner()
+        {
+            // Loop through each label to check if it is colored black yet.
+            foreach (Control control in this.tableLayoutPanel1.Controls)
+            {
+                Label curLabel = control as Label;
+                // Ensure the cast was successful.
+                if (curLabel != null)
+                {
+                    // Check if this label has been matched yet.
+                    if (curLabel.ForeColor != Color.Black)
+                    {
+                        // Not yet matched, no winner yet.
+                        return;
+                    }
+                }
+            }
+
+            // If all icons are black, user has won. Close the form.
+            MessageBox.Show("You matched all the icons!", "Congratulations");
+            this.Close();
         }
 
         /// <summary>
@@ -58,6 +89,12 @@ namespace MatchingGame
         /// <param name="e"></param>
         private void label_Click(object sender, EventArgs e)
         {
+            // Ignore any clicks when the timer is already enabled.
+            if (this.timer1.Enabled == true)
+            {
+                return;
+            }
+
             Label clickedLabel = sender as Label;
 
             if (clickedLabel != null)
@@ -67,9 +104,55 @@ namespace MatchingGame
                 {
                     return;
                 }
-                // Change the clicked label to black to reveal the icon.
+
+                // Check if this is the first label clicked, reveal the icon.
+                if (this.firstClicked == null)
+                {
+                    this.firstClicked = clickedLabel;
+                    // Change the clicked label to black to reveal the icon.
+                    clickedLabel.ForeColor = Color.Black;
+                    return;
+                }
+
+                // If it isn't the first label clicked, the second one has been clicked. Reveal its icon.
+                this.secondClicked = clickedLabel;
                 clickedLabel.ForeColor = Color.Black;
+
+                // Check here for all icons overturned (a check for winning case). 
+                this.CheckForWinner();
+
+                // Check here for matching icons.
+                if (this.firstClicked.Text == this.secondClicked.Text)
+                {
+                    // Found a match, so no need to start the timer.
+                    this.firstClicked = null;
+                    this.secondClicked = null;
+                    return;
+                }
+
+                // Since two icons are now shown, start the timer.
+                this.timer1.Start();
             }
         }
+
+        /// <summary>
+        /// This timer is started when the player selects two icons that do not match. It will 
+        /// run for 750ms then turn itself off and hide both selected icons.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Stop the timer.
+            this.timer1.Stop();
+            // Hide both icons.
+            this.firstClicked.ForeColor = this.firstClicked.BackColor;
+            this.secondClicked.ForeColor = this.secondClicked.BackColor;
+            // Reset the references to the currently-clicked icons.
+            this.firstClicked = null;
+            this.secondClicked = null;
+        }
+
+        
     }
 }
